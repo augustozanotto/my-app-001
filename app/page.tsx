adca1ca7507fe28cad6d7f9298efe4bc0ece111a
augustozanotto <augustozanotto@outlook.it>
2024-12-12 13:29:34 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 
+
+
 export default function Home() {
   return (
+    
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      window.location.reload();
         
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
@@ -13,7 +17,7 @@ export default function Home() {
             </code>
             
           </li>
-          <li>Save and see your changes instantly. NOW.</li>
+          <li>Save and see your changes instantly. CIN CION</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
