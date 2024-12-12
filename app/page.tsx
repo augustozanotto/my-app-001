a1be5cf296b3026c7223685383460344523d82f3
augustozanotto <augustozanotto@outlook.it>
2024-12-12 13:37:53 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
     
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      window.location.reload();
         
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
@@ -17,7 +16,7 @@ export default function Home() {
             </code>
             
           </li>
-          <li>Save and see your changes instantly. CIN CION</li>
+          <li>Save and see your changes instantly. CIN CION & CICCIO C.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
